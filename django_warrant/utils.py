04c3a0ec57058bf2c649dd64f23e588929736172
Collin Caram <collin.caram@gmail.com>
2019-04-15 13:44:58 -0500

    update requirements for django 1.11

--- django_warrant/utils.py
+++ django_warrant/utils.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from warrant import Cognito
 
 
-def cognito_to_dict(attr_list,mapping):
+def cognito_to_dict(attr_list, mapping):
     user_attrs = dict()
     for i in attr_list:
         name = mapping.get(i.get('Name'))
@@ -11,18 +11,19 @@ def cognito_to_dict(attr_list,mapping):
             user_attrs[name] = value
     return user_attrs
 
+
 def user_obj_to_django(user_obj):
     c_attrs = settings.COGNITO_ATTR_MAPPING
     user_attrs = dict()
-    for k,v in user_obj.__dict__.iteritems():
+    for k, v in user_obj.__dict__.iteritems():
         dk = c_attrs.get(k)
         if dk:
             user_attrs[dk] = v
     return user_attrs
 
-def get_cognito(request):
 
-    c = Cognito(settings.COGNITO_USER_POOL_ID,settings.COGNITO_APP_ID,
+def get_cognito(request):
+    c = Cognito(settings.COGNITO_USER_POOL_ID, settings.COGNITO_APP_ID,
                 access_token=request.session.get('ACCESS_TOKEN'),
                 id_token=request.session.get('ID_TOKEN'),
                 refresh_token=request.session.get('REFRESH_TOKEN'))
@@ -33,4 +34,3 @@ def get_cognito(request):
         request.session['ID_TOKEN'] = c.id_token
         request.session.save()
     return c
-
